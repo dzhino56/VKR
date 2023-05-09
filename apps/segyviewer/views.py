2621e17e8301ffd3c2d82e44d7a9618f663dea19
dzhino98 <pavlov@nposys.ru>
2023-05-09 16:15:53 +0700

    changelog: - убраны отладочные принты - добавлены значения сортировок в респонс - сделано кэширование трасс

--- apps/segyviewer/views.py
+++ apps/segyviewer/views.py
@@ -6,7 +6,6 @@ from django.core.cache import cache
 from django.http import JsonResponse, HttpResponse
 import segyio
 import pandas as pd
-import time
 import numpy as np
 from rest_framework.decorators import permission_classes
 from rest_framework.permissions import IsAuthenticated
@@ -39,8 +38,14 @@ class FileAPIView(APIView):
         return Response({'files': GetFilesSerializer(files, many=True).data})
 
     def post(self, request, format=None):
+        user = self.request.user
         file_obj = request.FILES['file']
         file_name = file_obj.name
+        try:
+            File.objects.get(user=user, name=file_name)
+            return Response({'message': 'Please, rename  file. File with such name exists'}, status=412)
+        except File.DoesNotExist:
+            pass
         user = self.request.user
         file_path = create_file_path(user.id, file_name)
         file = File(
@@ -64,48 +69,122 @@ def parse_trace_headers(segyfile):
     return trace_headers
 
 
-def trace_view_set(request):
-    file_id = int(request.GET['fileId'])
-    file = File.objects.get(pk=file_id)
-    start = time.time()
-    trace_headers = get_trace_headers(file)
-    end = time.time()
-    print("Чтение заголовков заняло: ", end - start)
-    start = time.time()
-    traces_and_samples = get_trace_raw_and_samples(file)
-    traces = traces_and_samples["traces"]
-    samples = traces_and_samples["samples"]
-    end = time.time()
-    print("Чтение всех трасс заняло: ", end - start)
-
-    return_headers = []
-
-    for key in request.GET:
+def get_request_params(get_values):
+    trace_settings = {
+        "group": {},
+        "sort": {}
+    }
+    for key in get_values:
         if key != 'sort' and key != 'fileId':
-            return_headers.append(key)
-            trace_headers = trace_headers.loc[
-                trace_headers[segyio.tracefield.keys[key]] == int(request.GET[key])
-                ]
+            trace_settings['group'][key] = get_values[key]
+
         elif key == 'sort':
-            split_data = request.GET[key].split('__', 1)
-            trace_headers = trace_headers.sort_values(
-                by=[segyio.tracefield.keys[split_data[0]]],
-                ascending=(split_data[1].lower() != 'desc')
-            )
-            return_headers.append(split_data[0])
-    index = trace_headers.index
-    return_data = []
-    for i in index:
-        return_data.append(traces[i].tolist())
+            split_data = get_values[key].split('__', 1)
+
+            trace_settings["sort"][split_data[0]] = split_data[1].lower()
+
+    my_keys = list(trace_settings["group"].keys())
+    my_keys.sort()
+
+    sorted_dict = {i: trace_settings["group"][i] for i in my_keys}
+    trace_settings["group"] = sorted_dict
+
+    return trace_settings
+
+
+def try_to_get_from_cache(cache_name):
+    if cache_name in cache:
+        return cache.get(cache_name)
+    else:
+        return None
+
+
+def create_cache_name(real_file_path, request_groups):
+    cache_name = real_file_path
+    for k, v in request_groups.items():
+        cache_name += '_' + k + '=' + v
+    return cache_name
+
+
+def get_return_data(request_params, file):
+    trace_headers = get_trace_headers(file)
+    for key, value in request_params["group"].items():
+        trace_headers = trace_headers.loc[
+            trace_headers[segyio.tracefield.keys[key]] == int(value)
+            ]
+
+    cache_name = create_cache_name(file.real_file_path, request_params['group'])
+    data_from_cache = try_to_get_from_cache(cache_name)
+
+    if data_from_cache is None:
+        traces_and_samples = get_trace_raw_and_samples(file)
+
+        traces = traces_and_samples["traces"]
+        samples = traces_and_samples["samples"]
+
+        index_and_values = get_sorted_indexes_and_values(request_params, trace_headers)
+        sorting_values = index_and_values['values']
+
+        return_data = []
+        data_for_cache = {
+            "traces": {},
+            "samples": [],
+            "index": index_and_values['index'].tolist()
+        }
+        for i in index_and_values["index"]:
+            return_data.append(traces[i])
+            data_for_cache["traces"][i] = traces[i].tolist()
+        data_for_cache["samples"] = samples.tolist()
+        print(index_and_values['index'])
+        dump = {k: v for k, v in data_for_cache.items()}
+
+        cache.set(cache_name, json.dumps(dump))
+
+    else:
+        traces_and_samples = json.loads(data_from_cache)
+
+        traces = traces_and_samples['traces']
+        samples = np.array(traces_and_samples["samples"])
+
+        index_and_values = get_sorted_indexes_and_values(request_params, trace_headers)
+        sorting_values = index_and_values['values']
+
+        return_data = []
+        for i in index_and_values["index"]:
+            return_data.append(traces[str(i)])
 
     np_array = np.array(return_data)
     np_array = np_array.transpose()
-    return_dict = {
+
+    return {
         "traces": np_array,
-        "samples": samples
+        "samples": samples,
+        "sorting": sorting_values
     }
 
-    return HttpResponse(json.dumps({k: v.tolist() for k, v in return_dict.items()}))
+
+def get_sorted_indexes_and_values(request_params, trace_headers):
+    for key, value in request_params["sort"].items():
+        header_for_sorting = segyio.tracefield.keys[key]
+        trace_headers = trace_headers.sort_values(
+            by=[header_for_sorting],
+            ascending=(value.lower() != 'desc')
+        )
+
+    return {
+        "index": trace_headers.index,
+        "values": trace_headers[header_for_sorting].values
+    }
+
+
+def trace_view_set(request):
+    file_id = int(request.GET['fileId'])
+    file = File.objects.get(pk=file_id)
+
+    request_params = get_request_params(request.GET)
+    return_data = get_return_data(request_params, file)
+
+    return HttpResponse(json.dumps({k: v.tolist() for k, v in return_data.items()}))
 
 
 @permission_classes([IsAuthenticated])
@@ -115,16 +194,17 @@ def headers_view(request):
 
 def get_trace_headers(file):
     filename = file.real_file_path
+    cache_object_name = filename + "_headers"
     with segyio.open(filename, ignore_geometry=True) as f:
-        if filename in cache:
-            serialized_headers = cache.get(filename)
+        if cache_object_name in cache:
+            serialized_headers = cache.get(cache_object_name)
             trace_headers = pd.read_json(serialized_headers)
             headers = f.header
             trace_headers.columns = headers[0].keys()
         else:
             trace_headers = parse_trace_headers(f)
             serialized_headers = trace_headers.to_json()
-            cache.set(filename, serialized_headers)
+            cache.set(cache_object_name, serialized_headers)
     return trace_headers
 
 
@@ -146,10 +226,7 @@ def get_trace_raw_and_samples(file):
 def column_unique_values(request):
     file_id = int(request.GET['fileId'])
     file = File.objects.get(pk=file_id)
-    start = time.time()
     trace_headers = get_trace_headers(file)
-    end = time.time()
-    print("Чтение заголовков заняло: ", end - start)
 
     values = trace_headers[segyio.tracefield.keys[request.GET['column']]].unique().tolist()
     values.sort()
